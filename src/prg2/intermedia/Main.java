3abe8f004ce4b8cd7b3119c14492633d10a79b65
hcastc00 <hcastc00@estudiantes.unileon.es>
2019-11-28 13:46:37 +0100

    Mejorado el tema de los Scanner entre clases con un Scanner público. //TODO comenter código y a poder ser filtrar parte de la entrada

--- src/prg2/intermedia/Main.java
+++ src/prg2/intermedia/Main.java
@@ -2,18 +2,19 @@ package prg2.intermedia;
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args){
         //Pido la entrada de una hoja
-        Scanner sc = new Scanner(System.in);
-        int numPages = sc.nextInt();
+        int numPages = Main.sc.nextInt();
         sc.nextLine();
 
-        //Check good input
+        //Compruebo numero de hojas validas
         if (numPages<=0) invalidImput();
 
         Page pages[] = new Page[numPages];
         for (int i = 0; i<numPages;i++){
-            pages[i] = new Page(sc);
+            pages[i] = new Page();
         }
         for (int i = 0; i<numPages;i++){
             pages[i].solve();
@@ -21,7 +22,7 @@ public class Main {
 
     }
     public static void invalidImput(){
-        System.out.println("Invalid Input");
+        System.err.println("Invalid Input");
         System.exit(-1);
     }
 }
--- src/prg2/intermedia/Page.java
+++ src/prg2/intermedia/Page.java
@@ -1,29 +1,22 @@
 package prg2.intermedia;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Page {
 
     private int rows;
     private int columns;
-    private Scanner sc;
+    private Scanner sc = Main.sc;
     private String[][] cells;
     private boolean[][] isFormula;
-    private Queue<Cell> formulas = new LinkedList<Cell>();
     private int[][] solution;
 
-    public Page(Scanner sc) {
-        this.sc = sc;
+    public Page() {
         readSize();
         cells = new String[this.rows][this.columns];
         isFormula = new boolean[this.rows][this.columns];
         solution = new int[this.rows][this.columns];
         readCells();
         getFormulas();
-
-
     }
 
     private void readSize() {
@@ -39,97 +32,115 @@ public class Page {
 
     private void readCells() {
         String line;
-        String [] lineCells;
+        String[] lineCells;
         for (int i = 0; i < this.rows; i++) {
-            line = sc.nextLine();
+            line = this.sc.nextLine();
             lineCells = line.split(" ");
-            if(lineCells.length!=this.columns) Main.invalidImput();
+            if (lineCells.length != this.columns) Main.invalidImput();
             this.cells[i] = lineCells;
         }
     }
 
     private void getFormulas() {
-        Cell cell;
-        for (int y = 0; y<this.rows; y++) {
-            for (int x = 0; x<this.columns; x++){
-                if (this.cells[y][x].charAt(0) == '='){
+        for (int y = 0; y < this.rows; y++) {
+            for (int x = 0; x < this.columns; x++) {
+                if (this.cells[y][x].charAt(0) == '=') {
                     this.isFormula[y][x] = true;
                 }
             }
         }
     }
 
-    public void solve(){
-      Cell form;
+    public void solve() {
+        Cell form;
 
-       for (int y=0; y<this.rows; y++){
-           for (int x=0; x<this.columns; x++){
-               if (isFormula[y][x]){
-                   form = new Cell(y,x);
-                   solution[y][x] = solveFormula(form);
-               }
-               else{
-                   solution[y][x] = Integer.parseInt(cells[y][x]);
-               }
-           }
-       }
-
-       print2D(solution);
+        for (int y = 0; y < this.rows; y++) {
+            for (int x = 0; x < this.columns; x++) {
+                if (isFormula[y][x]) {
+                    form = new Cell(y, x);
+                    solution[y][x] = solveFormula(form);
+                } else {
+                    solution[y][x] = Integer.parseInt(cells[y][x]);
+                }
+            }
+        }
+        printMat(solution);
     }
 
-    private int solveFormula(Cell form){
-        String formula = this.cells[form.getY()][form.getX()].replaceAll("=","");
+    private int solveFormula(Cell form) {
+        String formula = this.cells[form.getY()][form.getX()].replaceAll("=", "");
         String[] operators = formula.split("\\+");
         Cell coords;
         int value = 0;
-        for (int j=0; j<operators.length; j++){
-           coords = stringToCoords(operators[j]);
-            if (isFormula[coords.getY()][coords.getX()]){
+        for (int i = 0; i < operators.length; i++) {
+            String operator = operators[i];
+            coords = stringToCoords(operator);
+            if (isFormula[coords.getY()][coords.getX()]) {
                 value += solveFormula(coords);
-            }
-            else{
+            } else {
                 value += Integer.parseInt(cells[coords.getY()][coords.getX()]);
             }
         }
         return value;
     }
 
-
-    private Cell stringToCoords(String input){
+    private Cell stringToCoords(String input) {
         StringBuilder letters = new StringBuilder();
         StringBuilder numbers = new StringBuilder();
         Cell coords;
 
-        for (int i=0;i<input.length();i++) {
+        for (int i = 0; i < input.length(); i++) {
             if (Character.isDigit(input.charAt(i))) {
                 numbers.append(input.charAt(i));
             } else {
                 letters.append(input.charAt(i));
             }
         }
-        coords = new Cell(Integer.parseInt(numbers.toString())-1,lettersToNumber(letters.toString()));
+        coords = new Cell(Integer.parseInt(numbers.toString()) - 1, lettersToNumber(letters.toString()));
         return coords;
     }
 
-    private int lettersToNumber(String letters){
+    private int lettersToNumber(String letters) {
         letters = letters.toUpperCase();
         char[] lettersSplit = letters.toCharArray();
         int value = 0;
-        for (int i=0, b=lettersSplit.length-1;i<lettersSplit.length;i++,b--){
-            value += (lettersSplit[b] - 64) * Math.pow(26,i);
+  /*
+  Recorre el arry de letras de izda a derecha, es decir, de mas valor (exp mayor) a menos
+  Las letras mayusculas emiezan en A=64, le resto 64 para que A=0
+  Se hace un cambio de base, de base 26 a base 10
+   */
+        for (int exp = 0, pos = lettersSplit.length - 1; exp < lettersSplit.length; exp++, pos--) {
+            value += (lettersSplit[pos] - 64) * Math.pow(26, exp);
         }
         value--;
         return value;
     }
 
-    public static void print2D(int mat[][])
-    {
-        // Loop through all rows
-        for (int[] row : mat)
+    private void printMat(int[][] mat) {
+        for (int y = 0; y < this.rows; y++) {
+            for (int x = 0; x < this.columns; x++) {
+                System.out.print(mat[y][x]);
+                if (x == this.columns - 1) break; //No mete espacio al final de los ultimos numeros de cada fila
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+}
+//Clase cell utilizada para manejar coordenadas de forma mas comoda
+class Cell {
+    private int y, x;
 
-            // converting each row as string
-            // and then printing in a separate line
-            System.out.println(Arrays.toString(row));
+    public Cell(int y, int x) {
+        this.y = y;
+        this.x = x;
     }
 
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
 }
